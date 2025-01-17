96c42c024c7a942cd25308e2473a273eeb15bde8
Zhogin Ivan <mr.i-zhogin@yandex.ru>
2025-01-17 21:19:24 +0300

    fourth commit search done

--- composeApp/src/androidMain/kotlin/ru/zhogin/Previews.kt
+++ composeApp/src/androidMain/kotlin/ru/zhogin/Previews.kt
@@ -7,6 +7,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import ru.zhogin.book.domain.Book
+import ru.zhogin.book.presentation.book_list.BookListScreen
+import ru.zhogin.book.presentation.book_list.BookListState
 import ru.zhogin.book.presentation.book_list.components.BookSearchBar
 
 @Preview
@@ -27,3 +30,31 @@ private fun BookSearchBarPreview() {
     }
 
 }
+
+private val books = (1..100).map {
+    Book(
+        id = it.toString(),
+        title = "Book $it",
+        imageUrl = "https://test.com",
+        authors = listOf("Ivan Zhogin"),
+        description = "Description $it",
+        languages = emptyList(),
+        firstPublishYear = null,
+        averageRating = 4.67665,
+        ratingCount = 5,
+        numPages = 100,
+        numEditions = 3
+    )
+}
+
+@Preview
+@Composable
+private fun BookListScreenPreview() {
+    BookListScreen(
+        state = BookListState(
+            searchResults = books
+        ),
+        onAction = {}
+    )
+}
+
